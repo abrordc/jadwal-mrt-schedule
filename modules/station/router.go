b6c8ee7f5f7764224dfb4bb5037024f700ba98bf
abrordc <moh.abrorilhuda@gmail.com>
2024-11-18 09:21:08 +0800

    create:"struct data and code error"

--- modules/station/router.go
+++ modules/station/router.go
@@ -15,6 +15,10 @@ func Initiate(router *gin.RouterGroup) {
 	station.GET("", func(c *gin.Context) {
 		GetAllStation(c,stationService)
 	})
+
+	station.GET("/:id", func(c *gin.Context){
+		CheckSchedulesByStation(c, stationService)
+	})
 }
 
 func GetAllStation(c *gin.Context, service Service){
@@ -37,4 +41,30 @@ func GetAllStation(c *gin.Context, service Service){
 			Data: datas,
 		 },
 	)
+}
+
+func CheckSchedulesByStation(c *gin.Context, service Service) {
+	id := c.Param("id")
+
+	datas, err := service.CheckSchedulesByStation(id)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest,
+			response.APIResponse{
+			   Success: false,
+			   Message: err.Error(),
+			   Data: nil,
+			},
+	   )
+	   return
+	}
+
+	c.JSON(http.StatusOK,
+		response.APIResponse{
+		   Success: true,
+		   Message: "success get schedule by station",
+		   Data: datas,
+		},
+   )
+
 }
--- modules/station/service.go
+++ modules/station/service.go
@@ -1,6 +1,7 @@
 package station
 
 import (
+	"errors"
 	"net/http"
 	"time"
 
@@ -11,6 +12,7 @@ import (
 
 type Service interface {
 	GetAllStation() (response []StationResponse, err error)
+	CheckSchedulesByStation(id string) (response []ScheduleResponse, err error)
 }
 
 type service struct {
@@ -48,4 +50,55 @@ func(s *service) GetAllStation() (response []StationResponse, err error) {
 
 
 	return
+}
+
+func (s *service) CheckSchedulesByStation(id string) (response []ScheduleResponse, err error){
+	url := "https://www.jakartamrt.co.id/id/val/stasiuns"
+
+	//hit url
+	byteResponse,err := client.DoRequest(s.client,url) 
+
+	if err != nil {
+		return
+	}
+	var schedule []Schedule
+	err = json.Unmarshal(byteResponse,&schedule)
+	if err != nil {
+		return
+	}
+
+	//schedule selected by id station
+
+	var scheduleSelected Schedule
+	for _,item := range schedule {
+		if item.StationId == id {
+			scheduleSelected= item
+			break
+		}
+	}
+
+	if scheduleSelected.StationId == ""{
+		err = errors.New("station not found")
+		return
+	}
+
+	response, err = ConvertDataToResponse(scheduleSelected)
+	if err != nil {
+		return
+	}
+
+	return
+
+
+}
+
+func ConvertDataToResponse(schedule Schedule) (response []ScheduleResponse, err error){
+	var (
+		LebakBulusTripName = "Station Lebak Bulus Grab"
+		BundaranHITripName = "Station Bundaran HI Bank DKI"
+	)
+
+	scheduleLebakBulus := schedule.ScheduleLebakBulus
+	scheduleBundaranHI := schedule.ScheduleBundaranHi
+	
 }
